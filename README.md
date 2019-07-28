863b494768776648599499cc73cc8cd01efbb662
RedOrBluePill <llipeulbroder@gmail.com>
2019-07-28 19:33:35 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# redorbluepill.github.io
---
+++ precache-manifest.8642d2c4fe5892e5b205faaae39c52bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea0a94608aed7ee6cdaec30d10d6171",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db0d19747e964b839dc9",
+    "url": "/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "ebfc9c3183c1adfb9ea3",
+    "url": "/static/js/2.f0259060.chunk.js"
+  },
+  {
+    "revision": "db0d19747e964b839dc9",
+    "url": "/static/js/main.7caa09c9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
